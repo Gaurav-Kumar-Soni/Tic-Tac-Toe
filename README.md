bee1a5cec2243aff034e2e64f5883fd0aac1d39c
sonigaurav950 <108818110+sonigaurav950@users.noreply.github.com>
2023-02-12 08:16:18 +0530

    adding code files

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# Tic-Tac-Toe
-a simple tic-tac-toe game 
+# Tic Tac Toe Game
+
+A simple text-based tic-tac-toe game where two players can play against each other , written in C++. The game is played on a 3x3 grid, and the objective is to get three of your symbols in a row, either horizontally, vertically, or diagonally.
+
+## Getting Started
+
+This program is written in C++ and requires a C++ compiler to run. The code is intended to be run in the terminal.
+
+### Prerequisites
+
+You need to have a C++ compiler installed on your local machine.
+
+### Installation
+
+1. Clone or download the repository.
+2. Compile the source code using a C++ compiler.
+3. Run the compiled executable file.
+
+### Running the Game
+
+1. To run the game, simply compile the code using your preferred C++ compiler and run the executable file.
+2. The game starts with player 1 and player 2 alternately marking squares on the 3x3 grid.
+
+3. The program will prompt you for input for the co-ordinates (x, y) of your next move. The co-ordinates must be within the range of (1,1) to (3,3).
+
+4. The game will continue until either one of the players wins or there is a draw. The result of the game will be displayed at the end of the game.
+
+
+### Controls
+
+The game is controlled using the keyboard, with the player entering the coordinates of the square they wish to mark.
+
+### Built With
+
+- C++
+
+### Author
+
+@sonigaurav950
+
+### Example
+
+Here is an example of a game being played:
+Input Co-ordinates with a space and with in rage of [1,1] to [3,3].
+
+#### chance of player 1 : 1 1
+
+|X| | |
+| | | |
+| | | |
+
+#### chance of player 2 : 2 2
+
+|X| | |
+| |O| |
+| | | |
+
+#### chance of player 1 : 1 2
+
+|X|X| |
+| |O| |
+| | | |
+
+#### chance of player 2 : 3 2
+
+|X|X| |
+| |O|O|
+| | | |
+
+#### chance of player 1 : 1 3
+
+|X|X|X|
+| |O|O|
+| | | |
+
+### player 1 wins
---
+++ game.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool validInput(int x, int y, char arr[3][3]){
+    if ((x > 0 && x < 4) && (y > 0 && y < 4) && arr[x - 1][y - 1] == ' '){
+        return true;
+    }
+    return false;
+}
+
+void printBoard(char arr[3][3]){
+    for (int i = 0; i < 3; i++){
+        cout << '|';
+        for (int j = 0; j < 3; j++){
+            cout << arr[i][j] << '|';
+        }
+        cout << endl;
+    }
+}
+bool check_win(char arr[3][3]){
+    for (int i = 0; i < 3; i++){
+        if ((arr[i][0] == arr[i][1] && arr[i][1] == arr[i][2] && arr[i][2] != ' ') ||
+            (arr[0][i] == arr[1][i] && arr[1][i] == arr[2][i]) && arr[2][i] != ' ')
+            return true;
+    }
+
+    if ((arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2] && arr[2][2] != ' ') ||
+        (arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0] && arr[2][0] != ' '))
+        return true;
+
+    return false;
+}
+int main()
+{ 
+    cout<<"Input Co-ordinates with a space and with in rage of [1,1] to [3,3]."<<endl<<endl;
+    char arr[3][3] = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+    };
+    int count = 0;
+    int p;
+    int x, y;
+    int flag = 0;
+    while (!check_win(arr)){
+        if (count % 2 == 0){
+            p = 1;
+        }
+        else
+            p = 2;
+        if (count >= 9){
+            flag = 0;
+            break;
+        }
+        cout << "chance of player " << p << " : ";
+        cin >> x >> y;
+        if (!validInput(x, y, arr)){
+            cout << "Invalid input!" << endl;
+            continue;
+        }
+        if (p == 1){
+            arr[x - 1][y - 1] = 'X';
+        }
+        else
+            arr[x - 1][y - 1] = 'O';
+        count++;
+        printBoard(arr);
+        flag = 1;
+    }
+    if (flag)
+        cout << "player " << p << " win" << endl;
+    else
+        cout << "Draw!" << endl;
+    return 0;
+}
